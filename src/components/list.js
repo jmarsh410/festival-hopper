2725cbf702e25e8a9c6cd45d20e38790ca0c02dc
Jered Marshall <jmarsh410@gmail.com>
2017-04-08 13:25:19 -0500

    import lodash and use it to always flatten a list’s items in case it is an array of arrays

--- src/components/list.js
+++ src/components/list.js
@@ -1,12 +1,14 @@
 /* jshint ignore:start */
 
 import React, { Component } from 'react';
+import _ from 'lodash';
 import '../styles/list.css';
 
 class List extends Component {
   render() {
     const Type = this.props.type;
-    const html = this.props.items.map((item, i) => {
+    const items = _.flatten(this.props.items); // flatten the items a single level
+    const html = items.map((item, i) => {
       // if the item doesn't have an id property, then default to showing the array index
       const key = item.id ? item.id : i;
       return <li key={key} className="list-item"><Type index={i} data={item}/></li>;
