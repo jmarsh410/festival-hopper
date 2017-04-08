e1440f4938b4b20fb3904ff06df10218d8e06c6c
Jered Marshall <jmarsh410@gmail.com>
2017-04-07 19:30:15 -0500

    add index storage for all data lists

---
+++ src/data-lists/index.js
@@ -0,0 +1,7 @@
+import drafthouse from './drafthouse';
+import thePurge from './the-purge';
+const DataLists = {
+  [drafthouse.id]: drafthouse,
+  [thePurge.id]: thePurge,
+};
+export default DataLists;
