4362ff2ef2c9baa71c77936c9b8ac0025fed2412
Jered Marshall <jmarsh410@gmail.com>
2017-04-09 20:28:52 -0500

    only render the title if it is provided. flatten and dedupe any item arrays provided to list.js

--- src/components/list.js
+++ src/components/list.js
@@ -7,15 +7,20 @@ import '../styles/list.css';
 class List extends Component {
   render() {
     const Type = this.props.type;
-    const items = _.flatten(this.props.items); // flatten the items a single level
-    const html = items.map((item, i) => {
+    const flatItems = _.flatten(this.props.items); // flatten the items a single level
+    const deDupeItems = _.uniqBy(flatItems, 'id'); // re=move duplicate beers
+    const html = deDupeItems.map((item, i) => {
       // if the item doesn't have an id property, then default to showing the array index
       const key = item.id ? item.id : i;
       return <li key={key} className="list-item"><Type index={i} data={item}/></li>;
     });
+    let title = null;
+    if (this.props.title) {
+      title = (<h1 className="list-title">{ this.props.title }</h1>);
+    }
     return (
       <div className="list">
-        <h1 className="list-title">{ this.props.title }</h1>
+        {title}
         <ul className="list-main" onClick={this.props.onClick} onChange={this.props.onChange}>
           { html }
         </ul>
