fc1202d5d8afc55dd0caed38ec6f4d37da6cbcb8
Jered Marshall <jmarsh410@gmail.com>
2017-04-09 20:29:01 -0500

    created loading spinner

---
+++ src/components/loading-spinner.js
@@ -0,0 +1,14 @@
+/* jshint ignore:start */
+
+import React, { Component } from 'react';
+import '../styles/loading-spinner.css';
+
+class LoadingSpinner extends Component {
+  render(){
+    return (
+      <div className="loadingSpinner"></div>
+    );
+  }
+}
+
+export default LoadingSpinner;
