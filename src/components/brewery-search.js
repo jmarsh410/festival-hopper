07e61dba20deb0da5fc060e518b6d156d02e3597
Jered Marshall <jmarsh410@gmail.com>
2017-04-24 19:37:40 -0500

    add classes for styling

--- src/components/brewery-search.js
+++ src/components/brewery-search.js
@@ -5,6 +5,7 @@ import List from './list';
 import Brewery from './brewery';
 import '../styles/brewery-search.css';
 import '../styles/search.css';
+import '../styles/button.css';
 
 class BrewerySearch extends Component {
   render(){
@@ -18,7 +19,7 @@ class BrewerySearch extends Component {
       <div className="brewerySearch">
         <form className="search" onSubmit={this.props.onSubmit} noValidate>
           <input className="search-field" type="text" name="brewery-name" placeholder="Search Breweries"/>
-          <button className="search-btn" type="submit">Submit</button>
+          <button className="search-btn btn btn--positive" type="submit">Submit</button>
         </form>
         { breweries }
       </div>
