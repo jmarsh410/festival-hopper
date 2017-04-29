590acaec5da505082ddbe183ade0f763bf107176
Jered Marshall <jmarsh410@gmail.com>
2017-04-29 11:19:13 -0500

    allow for a centering class to be applied to the loading spinner

--- src/components/loading-spinner.js
+++ src/components/loading-spinner.js
@@ -5,8 +5,12 @@ import '../styles/loading-spinner.css';
 
 class LoadingSpinner extends Component {
   render(){
+    let classes = 'loadingSpinner';
+    if (this.props.centered) {
+      classes += ' loadingSpinner--centered';
+    }
     return (
-      <div className="loadingSpinner"></div>
+      <div className={classes}></div>
     );
   }
 }
