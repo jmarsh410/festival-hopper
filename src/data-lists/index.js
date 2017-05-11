eabd010b2035acdc27759617b42436df64efaeee
Jered Marshall <jmarsh410@gmail.com>
2017-05-11 18:45:42 -0500

    add index fest list to the data-lists storage. can access by going to /curated/index-fest

--- src/data-lists/index.js
+++ src/data-lists/index.js
@@ -1,8 +1,8 @@
 import abw6th from './abw6th';
-import index3 from './index3';
+import indexFest from './indexFest';
 // key names must match the corresponding :listId url parameter
 const DataLists = {
   'abw-6th-anniversary': abw6th,
-  'index-fest': index3,
+  'index-fest': indexFest,
 };
 export default DataLists;
