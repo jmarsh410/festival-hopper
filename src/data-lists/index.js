f193464c41560eacadf5ab6e546e103b226d4fec
Jered Marshall <jmarsh410@gmail.com>
2017-06-03 10:22:51 -0500

    added/removed/edited some of the data-lists

--- src/data-lists/index.js
+++ src/data-lists/index.js
@@ -1,8 +1,6 @@
-import abw6th from './abw6th';
 import indexFest from './indexFest';
 // key names must match the corresponding :listId url parameter
 const DataLists = {
-  'abw-6th-anniversary': abw6th,
   'index-fest': indexFest,
 };
 export default DataLists;
